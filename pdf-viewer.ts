6e8302f37326959fcc90716fa54b76534496b7aa
Aria Haghighi <me@aria42.com>
2015-08-12 14:50:48 -0700

    Basic working sections

--- pdf-viewer.ts
+++ pdf-viewer.ts
@@ -15,8 +15,15 @@ module StructuredPDFViewer {
     }
 
     advancePanel(): boolean {
-      this.pageIdx += 1
-      this.panelIdx = 0
+      var numPanels = this.pdfData.pages[this.pageIdx].panelLayout.panels.length
+      if (this.panelIdx + 1 < numPanels) {
+        this.panelIdx += 1
+      } else if (this.pageIdx + 1 < this.pdfData.numPages) {
+        this.pageIdx += 1
+        this.panelIdx = 0
+      } else {
+        return false
+      }
       this.rerenderPanel()
       return true
     }
@@ -25,23 +32,42 @@ module StructuredPDFViewer {
       var pd: PDFStructure.PageData = this.pdfData.pages[this.pageIdx]
       var panel: PDFStructure.Panel = pd.panelLayout.panels[this.panelIdx]
       var canvas = this.displayParams.canvasElem
-      var canvasParentWidth = window.getComputedStyle(canvas.parentElement).width.replace('px','')      
-      var scale = parseFloat(canvasParentWidth) / pd.page.getViewport(1.0).width;
-      var viewport = pd.page.getViewport(scale);
+      var canvasParentWidth = window.getComputedStyle(canvas.parentElement).width.replace('px','')
+      var origViewport = pd.page.getViewport(1.0)
+      if (panel.type == PDFStructure.PanelType.LeftColumn ||
+          panel.type == PDFStructure.PanelType.RightColumn) {
+        origViewport.width = origViewport.width/2
+        origViewport.height = panel.height()
+      }
+      if (panel.type == PDFStructure.PanelType.TopHeader) {
+        origViewport.height = panel.height()
+      }
+      var scale = parseFloat(canvasParentWidth) / origViewport.width
+      var viewport = pd.page.getViewport(scale)
+      if (panel.type == PDFStructure.PanelType.TopHeader) {
+        viewport.height = scale * panel.height()
+      }
       canvas.width = window.devicePixelRatio * viewport.width
-      canvas.height = window.devicePixelRatio *viewport.height
+      canvas.height = window.devicePixelRatio * viewport.height
       var horizStrech = window.devicePixelRatio
       var verticStrech = window.devicePixelRatio
       canvas.style.width = viewport.width + 'px'
       canvas.style.height = viewport.height + 'px'
       var canvasCtx = canvas.getContext('2d')
-      canvasCtx.setTransform(horizStrech,0,0, verticStrech,0, 0)
+      var dx = 0
+      var dy = 0
+      if (panel.type == PDFStructure.PanelType.TopHeader) {
+        dy = (canvas.height - panel.height())/2
+      }
+      if (panel.type == PDFStructure.PanelType.RightColumn) {
+        dx = -canvas.width/2
+      }
+      canvasCtx.setTransform(horizStrech,0,0, verticStrech, dx, dy)
       var renderContext = {
         canvasContext: canvasCtx,
         viewport: viewport
       }
       pd.page.render(renderContext)
-      debugger
     }
   }
 
